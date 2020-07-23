11474cf15a6119e08d6da15f71058ffbfcf2614e
Seppe Gadeyne <seppe@fushia.be>
2020-07-23 13:23:47 +0200

    Update register-service-worker.js

--- register-service-worker.js
+++ register-service-worker.js
@@ -1,1 +1,13 @@
+const addToCache = async (pages) => {
+    const pageCache = await window.caches.open('page-cache');
+    await pageCache.addAll(pages);
+}
 
+// Check that service workers are registered
+if ('serviceWorker' in navigator) {
+    // Use the window load event to keep the page load performant
+    window.addEventListener('load', () => {
+        addToCache([window.location.pathname]);
+        navigator.serviceWorker.register('/service-worker.js');
+    });
+}
